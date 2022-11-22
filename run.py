0ba165c46d1f417f44c97cd2445c4bbba3cece91
SuvvadaSonia <suvvadasonia@gmail.com>
2022-11-22 19:26:08 +0530

    Revised code

--- run.py
+++ run.py
@@ -10,18 +10,6 @@ CopyRights - Tessrac Innovations Pvt Ltd.
 
 """
 
-# import winrm
-
-# session = winrm.Session('192.168.0.103', auth = ('vinod', 'Do0ordie17k'))
-# s = winrm.Session('61.16.142.236', auth=('root', 'India@321'))
-# s = winrm.Session('61.16.142.236', auth=('sonia.s', 'India@321'))
-# output = session.run_cmd('ipconfig', ['/all'])
-
-# print(output)
-
-# s = winrm.Session('192.168.43.193', auth=('DESKTOP-KITTORC', 'Sonia@5355'))
-# s = winrm.Session('192.168.137.103', auth=('Varalaxmi', 'Varalaxmi'))
-
 import winrm
 s = winrm.Session('192.168.1.105', auth=('Administrator', 'India@123'))
 ip_config = s.run_cmd('ipconfig', ['/all'])         # for ip configuration
