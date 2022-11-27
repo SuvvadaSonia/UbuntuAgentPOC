96ad1e1b60657c488bcaf51d530fa1b3ea00fa12
SuvvadaSonia <suvvadasonia@gmail.com>
2022-11-27 21:23:21 +0530

    commits

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 """ 
-
 Ubuntu Agent POC 
-Author by: Suvvada Sonia
 
 It connects to windows machine using WinRm
 and executes installer & uninstaller scripts
@@ -12,21 +10,52 @@ CopyRights - Tessrac Innovations Pvt Ltd.
 
 """
 
-import winrm  # WinRM allows you to perform various management tasks remotely.
-
-s = winrm.Session('192.168.1.105', auth=('Administrator', 'India@123'))
-ip_config = s.run_cmd('ipconfig', ['/all'])         # for ip configuration
-cpu_util = s.run_cmd('wmic cpu get loadpercentage')          # for cpu utilization
-disk_space = s.run_cmd('wmic diskdrive get size')             # for disk space
-ram = s.run_cmd('wmic computersystem get totalphysicalmemory')     # for RAM size
+from winrm import Session  # WinRM allows you to perform various management tasks remotely.
 
 
-print((ip_config.std_out).strip())
-print("#############################")
-print((cpu_util.std_out).strip())
-print("#############################")
-print((disk_space.std_out).strip())
-print("#############################")
-print((ram.std_out).strip())
+class UbuntuAgent:
+    
+    def __init__(self, target:str, auth:tuple) -> None:
+        try:
+            _user, _pwd = auth
+
+            self.__conn = Session(target, auth=(_user, _pwd))
+        except (OSError, ConnectionError) as e:
+            print(e)
+
+    def getConfig(self):
+        try:
+            result = self.__conn.run_cmd('ipconfig', ['/all'])
+            return result.std_out
+
+        except (OSError, ConnectionError) as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    IP = '192.168.1.105'
+    HOSTNAME = 'Administrator'
+    PWD = 'India@123'
+    r = UbuntuAgent(IP, (HOSTNAME, PWD)).getConfig()
+
+    print(r)
+
+
+
+
+# s = winrm.Session('192.168.1.105', auth=('Administrator', 'India@123'))
+# ip_config = s.run_cmd('ipconfig', ['/all'])         # for ip configuration
+# cpu_util = s.run_cmd('wmic cpu get loadpercentage')          # for cpu utilization
+# disk_space = s.run_cmd('wmic diskdrive get size')             # for disk space
+# ram = s.run_cmd('wmic computersystem get totalphysicalmemory')     # for RAM size
+
+
+# print((ip_config.std_out).strip())
+# print("#############################")
+# print((cpu_util.std_out).strip())
+# print("#############################")
+# print((disk_space.std_out).strip())
+# print("#############################")
+# print((ram.std_out).strip())
 
 
